627be391d41389fe7da8c7a45d54684f699bd754
Pyosangwoo <psw3619@naver.com>
2023-04-13 21:44:00 +0900

    feat. Order Info API
    - 주문 상세 정보 조회 API 개발

--- backend/src/main/java/com/metapop/backend/controller/OrdersController.java
+++ backend/src/main/java/com/metapop/backend/controller/OrdersController.java
@@ -1,11 +1,15 @@
 package com.metapop.backend.controller;
 
 import com.metapop.backend.dto.OrdersDTO.OrdersSaveDTO;
+import com.metapop.backend.entity.Orders;
+import com.metapop.backend.entity.Product;
 import com.metapop.backend.service.OrdersService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Tag(name = "order", description = "주문 API")
 @RestController
@@ -20,5 +24,11 @@ public class OrdersController {
     @PostMapping("/register")
     public String registration(@RequestBody OrdersSaveDTO ordersSaveDTO) {
         return ordersService.registration(ordersSaveDTO);
+    }
+
+    @Operation(summary = "", description = "주문 상세 정보 조회 API")
+    @GetMapping("/info/detail/{orders_id}")
+    public Optional<Orders> infodetail(@PathVariable Long orders_id) {
+        return ordersService.infodetail(orders_id);
     }
 }
--- backend/src/main/java/com/metapop/backend/service/OrdersService.java
+++ backend/src/main/java/com/metapop/backend/service/OrdersService.java
@@ -1,11 +1,14 @@
 package com.metapop.backend.service;
 
 import com.metapop.backend.dto.OrdersDTO.OrdersSaveDTO;
+import com.metapop.backend.entity.Orders;
+import com.metapop.backend.entity.Product;
 import com.metapop.backend.repository.OrdersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,5 +20,9 @@ public class OrdersService {
     public String registration(OrdersSaveDTO ordersSaveDTO) {
         ordersRepository.save(ordersSaveDTO.toEntity());
         return "주문 저장 완료";
+    }
+
+    public Optional<Orders> infodetail(Long orders_id) {
+        return ordersRepository.findById(orders_id);
     }
 }
