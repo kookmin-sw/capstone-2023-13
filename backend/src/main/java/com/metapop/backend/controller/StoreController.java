f1525daa2d937247b8d21ddd31a8932a52e077e6
Pyosangwoo <psw3619@naver.com>
2023-04-20 18:13:11 +0900

    fix. CORS Error
    - WebCofig 추가
    - 43.201.210.173 도메인 추가

--- backend/src/main/java/com/metapop/backend/controller/StoreController.java
+++ backend/src/main/java/com/metapop/backend/controller/StoreController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "store", description = "상점 API")
 @RestController
 @RequestMapping("stores")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://43.201.210.173")
 public class StoreController {
 
     @Autowired
--- backend/src/main/java/com/metapop/backend/entity/Store.java
+++ backend/src/main/java/com/metapop/backend/entity/Store.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "store")
