ab3e78033daabdadf09cb419a30f6f8f62218e6c
KangJuSeong <wntjd3686@gmail.com>
2023-04-26 15:32:24 +0900

    refact. message class 적용

--- websocket/server/message.py
+++ websocket/server/message.py
@@ -4,6 +4,23 @@ class Message:
     ACTION = "action"
     
     @classmethod
+    def get_init_data(cls, data):
+        user = data.get('user_id')
+        channel = data.get('channel_id')
+        X = data.get('X')
+        Y = data.get('Y')
+        return user, channel, X, Y
+    
+    @classmethod
+    def set_init_data(cls, user_id, channel_id, X, Y):
+        return {
+            "user_id": user_id,
+            "channel_id": channel_id,
+            "X": X,
+            "Y": Y
+        }
+    
+    @classmethod
     def connect(cls, user_id, status):
         return {
             "type": cls.CONNECT,
@@ -27,3 +44,8 @@ class Message:
             "X": X,
             "Y": Y
         }
+    @classmethod
+    async def broadcast(cls, app, channel, message):
+        for user, ws in app['websockets'][channel].items():
+            await ws.send_json(message)
+    
--- websocket/server/my_socket.py
+++ websocket/server/my_socket.py
@@ -16,11 +16,8 @@ async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
     
-    req = await ws.receive_json()
-    user = req.get('user_id')
-    channel = req.get('channel_id')
-    X = req.get('X')
-    Y = req.get('Y')
+    data = await ws.receive_json()
+    user, channel, X, Y = M.get_init_data(data)
     
     for user_id, socket in request.app['websockets'][channel].items():
         if user_id == user:
@@ -29,34 +26,34 @@ async def websocket_handler(request):
             await ws.close()
             return ws
     await ws.send_json(M.connect(user, 200))
-    await broadcast(request.app,
-                    channel,
-                    M.action(user, X, Y))
+    await M.broadcast(request.app,
+                      channel,
+                      M.action(user, X, Y))
     request.app['websockets'][channel][user] = ws
-    await broadcast(request.app,
-                    channel,
-                    M.chat("SERVER", f"[{user}] enter chat room"))
+    await M.broadcast(request.app,
+                      channel,
+                      M.chat("SERVER", f"[{user}] enter chat room"))
     async for msg in ws:
         if msg.type == web.WSMsgType.text:
             req = msg.json()
             if req.get('type') == 'action':
                 X = req.get('X')
                 Y = req.get('Y')
-                await broadcast(request.app,
-                                channel,
-                                M.action(user, X, Y))
+                await M.broadcast(request.app,
+                                  channel,
+                                  M.action(user, X, Y))
             elif req.get('type') == 'chat':
-                await broadcast(request.app,
-                                channel,
-                                M.chat(user, req.get('msg')))
+                await M.broadcast(request.app,
+                                  channel,
+                                  M.chat(user, req.get('msg')))
 
     del request.app['websockets'][channel][user]
     print(f"Disconnect {user}")
     return ws
 
-async def broadcast(app, channel, message):
-    for user, ws in app['websockets'][channel].items():
-        await ws.send_json(message)
+# async def broadcast(app, channel, message):
+#     for user, ws in app['websockets'][channel].items():
+#         await ws.send_json(message)
 
 
 async def web_server():
