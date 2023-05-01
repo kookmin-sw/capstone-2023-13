4f7034ace817e3d3d18db6554dda026803636a97
Pyosangwoo <psw3619@naver.com>
2023-05-01 23:51:39 +0900

    fix. allowedOrigin

--- backend/src/main/java/com/metapop/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/metapop/backend/config/SecurityConfig.java
@@ -9,37 +9,49 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
-
-    // authenticationManager를 Bean 등록합니다.
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //허용할 url 설정
+        configuration.addAllowedOrigin("http://43.201.210.173:80");
+        configuration.addAllowedOrigin("http://localhost:5173");
+        //허용할 헤더 설정
+        configuration.addAllowedHeader("*");
+        //허용할 http method
+        configuration.addAllowedMethod("*");
+        //사용자 자격 증명이 지원되는지 여부
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()// 요청에 대한 사용권한 체크
-                .antMatchers("/users/logout").authenticated()
-                .antMatchers("/users/info/**").authenticated()
-                .antMatchers("/users/update/**").authenticated()
-                .antMatchers("/users/findpw").authenticated()
-                .antMatchers("/users/myinfo").authenticated()
-                .antMatchers("/users/myinfo").authenticated()
-                .antMatchers("/users/send/**").authenticated()
-                .antMatchers("/**").permitAll()
-                .and()
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource());
+        http.csrf().disable()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                        UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests((authz)->authz
+                .antMatchers("/auth/**").authenticated()
+                .anyRequest().permitAll());
+
+        return http.build();
     }
+
 }
