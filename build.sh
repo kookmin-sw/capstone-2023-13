810af5d80357713f4ca618cc9f5a97cffca2c304
KangJuSeong <wntjd3686@gmail.com>
2023-05-04 18:50:31 +0900

    ApplicationStart hook에서 build.sh 로 docker build 후 실행

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# Front Update
 cd /home/ec2-user/capstone-2023-13
-#cp -r frontend/dist /usr/share/nginx/html/
+
+# Front Update
+cp -r frontend/dist /usr/share/nginx/html/
 
 # Backend Update
-#docker build -t backend backend/.
-#docker rm -f backend
-#docker run -d -p 8080:8080 --restart unless-stopped --name backend backend:latest
+docker build -t backend backend/.
+docker rm -f backend
+docker run -d -p 8080:8080 --restart unless-stopped --name backend backend:latest
 
 # Websocket Update
-#docker build -t websocket websocket/.
-#docker rm -f websocket
-#docker run -d -p 8081:8080 --restart unless-stopped --name websocket websocket:latest
+docker build -t websocket websocket/.
+docker rm -f websocket
+docker run -d -p 8081:8080 --restart unless-stopped --name websocket websocket:latest
