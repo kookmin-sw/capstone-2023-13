5d9f0d645dc8a0756f17fabbac04c41765d63db1
KangJuSeong <wntjd3686@gmail.com>
2023-05-04 17:48:49 +0900

    Docker script error 해결

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 cp -r frontend/dist /usr/share/nginx/html/
 
 # Backend Update
-docker build -t backend:latest backend/.
+docker build -t backend backend/.
 docker rm -f backend
 docker run -d -p 8080:8080 --restart unless-stopped --name backend backend:latest
 
 # Websocket Update
-docker build -t websocket:latest websocket/.
+docker build -t websocket websocket/.
 docker rm -f websocket
 docker run -d -p 8081:8080 --restart unless-stopped --name websocket websocket:latest
