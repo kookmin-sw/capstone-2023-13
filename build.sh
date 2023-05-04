ace6a7ddd98cbf70cda9fef3222ded14a990b4cf
KangJuSeong <wntjd3686@gmail.com>
2023-05-04 18:48:13 +0900

    appspec.yml ApllicationStart test

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 # Front Update
-cp -r frontend/dist /usr/share/nginx/html/
+cd /home/ec2-user/capstone-2023-13
+#cp -r frontend/dist /usr/share/nginx/html/
 
 # Backend Update
-docker build -t backend backend/.
-docker rm -f backend
-docker run -d -p 8080:8080 --restart unless-stopped --name backend backend:latest
+#docker build -t backend backend/.
+#docker rm -f backend
+#docker run -d -p 8080:8080 --restart unless-stopped --name backend backend:latest
 
 # Websocket Update
-docker build -t websocket websocket/.
-docker rm -f websocket
-docker run -d -p 8081:8080 --restart unless-stopped --name websocket websocket:latest
+#docker build -t websocket websocket/.
+#docker rm -f websocket
+#docker run -d -p 8081:8080 --restart unless-stopped --name websocket websocket:latest
