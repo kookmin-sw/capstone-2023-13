3418488c735a220eb393956eec67675cc43b8727
htchoi1006 <htchoi1006@kookmin.ac.kr>
2023-05-09 22:52:07 +0900

    add. react에 export된 gdevelop 파일추가

---
+++ frontend/public/maingdevelop/gdjs-evtsext__websocketclient__finalizeevent-func.js
@@ -0,0 +1,84 @@
+
+if (typeof gdjs.evtsExt__WebSocketClient__FinalizeEvent !== "undefined") {
+  gdjs.evtsExt__WebSocketClient__FinalizeEvent.registeredGdjsCallbacks.forEach(callback =>
+    gdjs._unregisterCallback(callback)
+  );
+}
+
+gdjs.evtsExt__WebSocketClient__FinalizeEvent = {};
+
+
+gdjs.evtsExt__WebSocketClient__FinalizeEvent.userFunc0x8d2100 = function(runtimeScene, eventsFunctionContext) {
+"use strict";
+gdjs.evtTools.wsClient.events.pop();
+
+};
+gdjs.evtsExt__WebSocketClient__FinalizeEvent.eventsList0 = function(runtimeScene, eventsFunctionContext) {
+
+{
+
+
+gdjs.evtsExt__WebSocketClient__FinalizeEvent.userFunc0x8d2100(runtimeScene, typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined);
+
+}
+
+
+};
+
+gdjs.evtsExt__WebSocketClient__FinalizeEvent.func = function(runtimeScene, parentEventsFunctionContext) {
+var eventsFunctionContext = {
+  _objectsMap: {
+},
+  _objectArraysMap: {
+},
+  _behaviorNamesMap: {
+},
+  getObjects: function(objectName) {
+    return eventsFunctionContext._objectArraysMap[objectName] || [];
+  },
+  getObjectsLists: function(objectName) {
+    return eventsFunctionContext._objectsMap[objectName] || null;
+  },
+  getBehaviorName: function(behaviorName) {
+    return eventsFunctionContext._behaviorNamesMap[behaviorName] || behaviorName;
+  },
+  createObject: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    if (objectsList) {
+      const object = parentEventsFunctionContext ?
+        parentEventsFunctionContext.createObject(objectsList.firstKey()) :
+        runtimeScene.createObject(objectsList.firstKey());
+      if (object) {
+        objectsList.get(objectsList.firstKey()).push(object);
+        eventsFunctionContext._objectArraysMap[objectName].push(object);
+      }
+      return object;    }
+    return null;
+  },
+  getInstancesCountOnScene: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    let count = 0;
+    if (objectsList) {
+      for(const objectName in objectsList.items)
+        count += parentEventsFunctionContext ?
+parentEventsFunctionContext.getInstancesCountOnScene(objectName) :
+        runtimeScene.getInstancesCountOnScene(objectName);
+    }
+    return count;
+  },
+  getLayer: function(layerName) {
+    return runtimeScene.getLayer(layerName);
+  },
+  getArgument: function(argName) {
+    return "";
+  },
+  getOnceTriggers: function() { return runtimeScene.getOnceTriggers(); }
+};
+
+
+gdjs.evtsExt__WebSocketClient__FinalizeEvent.eventsList0(runtimeScene, eventsFunctionContext);
+
+return;
+}
+
+gdjs.evtsExt__WebSocketClient__FinalizeEvent.registeredGdjsCallbacks = [];
---
+++ frontend/public/maingdevelop/gdjs-evtsext__websocketclient__onerror-func.js
@@ -0,0 +1,84 @@
+
+if (typeof gdjs.evtsExt__WebSocketClient__OnError !== "undefined") {
+  gdjs.evtsExt__WebSocketClient__OnError.registeredGdjsCallbacks.forEach(callback =>
+    gdjs._unregisterCallback(callback)
+  );
+}
+
+gdjs.evtsExt__WebSocketClient__OnError = {};
+
+
+gdjs.evtsExt__WebSocketClient__OnError.userFunc0x8d2100 = function(runtimeScene, eventsFunctionContext) {
+"use strict";
+eventsFunctionContext.returnValue = !!gdjs.evtTools.wsClient.lastError;
+
+};
+gdjs.evtsExt__WebSocketClient__OnError.eventsList0 = function(runtimeScene, eventsFunctionContext) {
+
+{
+
+
+gdjs.evtsExt__WebSocketClient__OnError.userFunc0x8d2100(runtimeScene, typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined);
+
+}
+
+
+};
+
+gdjs.evtsExt__WebSocketClient__OnError.func = function(runtimeScene, parentEventsFunctionContext) {
+var eventsFunctionContext = {
+  _objectsMap: {
+},
+  _objectArraysMap: {
+},
+  _behaviorNamesMap: {
+},
+  getObjects: function(objectName) {
+    return eventsFunctionContext._objectArraysMap[objectName] || [];
+  },
+  getObjectsLists: function(objectName) {
+    return eventsFunctionContext._objectsMap[objectName] || null;
+  },
+  getBehaviorName: function(behaviorName) {
+    return eventsFunctionContext._behaviorNamesMap[behaviorName] || behaviorName;
+  },
+  createObject: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    if (objectsList) {
+      const object = parentEventsFunctionContext ?
+        parentEventsFunctionContext.createObject(objectsList.firstKey()) :
+        runtimeScene.createObject(objectsList.firstKey());
+      if (object) {
+        objectsList.get(objectsList.firstKey()).push(object);
+        eventsFunctionContext._objectArraysMap[objectName].push(object);
+      }
+      return object;    }
+    return null;
+  },
+  getInstancesCountOnScene: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    let count = 0;
+    if (objectsList) {
+      for(const objectName in objectsList.items)
+        count += parentEventsFunctionContext ?
+parentEventsFunctionContext.getInstancesCountOnScene(objectName) :
+        runtimeScene.getInstancesCountOnScene(objectName);
+    }
+    return count;
+  },
+  getLayer: function(layerName) {
+    return runtimeScene.getLayer(layerName);
+  },
+  getArgument: function(argName) {
+    return "";
+  },
+  getOnceTriggers: function() { return runtimeScene.getOnceTriggers(); }
+};
+
+
+gdjs.evtsExt__WebSocketClient__OnError.eventsList0(runtimeScene, eventsFunctionContext);
+
+return !!eventsFunctionContext.returnValue;
+}
+
+gdjs.evtsExt__WebSocketClient__OnError.registeredGdjsCallbacks = [];
