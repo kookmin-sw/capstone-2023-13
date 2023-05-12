dba5a6efefce0e3518876989caa63d96f92f5f23
KangJuSeong <wntjd3686@gmail.com>
2023-05-12 21:34:40 +0900

    fix. 비정상 종료 유저 예외 처리

--- websocket/server/my_logging.py
+++ websocket/server/my_logging.py
@@ -27,3 +27,6 @@ class Logging:
 
     def action_logging(self, user_id, nickname, channel_id, direction, X, Y, Z):
         self.logger.debug(self.format('action', user_id, nickname, channel_id, f"{direction}, {X}, {Y}, {Z}"))
+
+    def basic_logging(self, msg):
+        self.logger.debug(msg + "\n")
