78b27e509920b13af7ac189e2691b356cb4179c3
Pyosangwoo <psw3619@naver.com>
2023-05-14 19:35:10 +0900

    test. environment variable

--- backend/src/main/java/com/metapop/backend/config/AwsS3Config.java
+++ backend/src/main/java/com/metapop/backend/config/AwsS3Config.java
@@ -23,6 +23,8 @@ public class AwsS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
+        System.out.println(accessKey);
+        System.out.println(secretKey);
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
