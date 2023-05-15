d0ff5fddb3d9a0f4141ab334c0f690af06000392
Pyosangwoo <psw3619@naver.com>
2023-05-15 22:00:52 +0900

    feat. Product amount decrease
    - Orders 생성시 Product amount 감소하는 로직 구현

--- backend/src/main/java/com/metapop/backend/service/OrdersService.java
+++ backend/src/main/java/com/metapop/backend/service/OrdersService.java
@@ -3,8 +3,11 @@ package com.metapop.backend.service;
 import com.metapop.backend.dto.OrdersDTO.OrdersSaveDTO;
 import com.metapop.backend.dto.OrdersDTO.OrdersUpdateDTO;
 import com.metapop.backend.entity.Orders;
+import com.metapop.backend.entity.Product;
 import com.metapop.backend.repository.OrdersRepository;
+import com.metapop.backend.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,10 +20,21 @@ import java.util.Optional;
 public class OrdersService {
 
     private final OrdersRepository ordersRepository;
+    private final ProductRepository productRepository;
 
-    public String registration(OrdersSaveDTO ordersSaveDTO) {
+    public ResponseEntity<String> registration(OrdersSaveDTO ordersSaveDTO) {
+        for (Long productId: ordersSaveDTO.getProductListId()) {
+            Optional<Product> Opproduct = productRepository.findById(productId);
+            Product product = Opproduct.orElseThrow();
+            if(product.getAmount() == 0) {
+                    return ResponseEntity.status(400).body("주문 할 양이 없습니다.");
+            }
+            else {
+                product.setAmount(product.getAmount() - 1);
+            }
+        }
         ordersRepository.save(ordersSaveDTO.toEntity());
-        return "주문 저장 완료";
+        return ResponseEntity.ok("주문 저장 완료");
     }
 
     public Optional<Orders> infodetail(Long orders_id) {
