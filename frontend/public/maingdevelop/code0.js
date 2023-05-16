74c3a47939762813c170c675876ec34ee6a582a4
Yeonoc <chlqhtjr752@kookmin.ac.kr>
2023-05-16 23:44:11 +0900

    fix. 캐릭터 씬 이동 시 삭제 에러 해결

--- frontend/public/maingdevelop/code0.js
+++ frontend/public/maingdevelop/code0.js
@@ -573,7 +573,7 @@ gdjs.Character_32custom_32sceneCode.GDBlueBarObjects1= [];
 gdjs.Character_32custom_32sceneCode.GDBlueBarObjects2= [];
 
 
-gdjs.Character_32custom_32sceneCode.userFunc0x146f9a0 = function(runtimeScene) {
+gdjs.Character_32custom_32sceneCode.userFunc0xb245f0 = function(runtimeScene) {
 "use strict";
 var logintoken = localStorage.getItem('login-token');
 
@@ -624,12 +624,12 @@ gdjs.Character_32custom_32sceneCode.eventsList0 = function(runtimeScene) {
 {
 
 
-gdjs.Character_32custom_32sceneCode.userFunc0x146f9a0(runtimeScene);
+gdjs.Character_32custom_32sceneCode.userFunc0xb245f0(runtimeScene);
 
 }
 
 
-};gdjs.Character_32custom_32sceneCode.userFunc0x15f0df0 = function(runtimeScene) {
+};gdjs.Character_32custom_32sceneCode.userFunc0x104d2e0 = function(runtimeScene) {
 "use strict";
 const userBody = runtimeScene.getGame().getVariables().get("UserBody")._str;
 const userEye = runtimeScene.getGame().getVariables().get("UserEye")._str;
@@ -740,7 +740,7 @@ gdjs.Character_32custom_32sceneCode.eventsList1 = function(runtimeScene) {
 {
 
 
-gdjs.Character_32custom_32sceneCode.userFunc0x15f0df0(runtimeScene);
+gdjs.Character_32custom_32sceneCode.userFunc0x104d2e0(runtimeScene);
 
 }
 
